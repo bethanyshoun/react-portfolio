96453722afd29586e222bd932efe32d96ae61c68
bethanyshoun <bethanyshoun@gmail.com>
2022-10-05 19:57:19 -0400

    updated App.js to map over categories

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -7,15 +8,39 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div className="App">
-      <Nav />
       <Header name="Hello" />
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
 
       <main>
-        <About />
+      {!contactSelected ? (
+          <>
         <Contact></Contact>
         <Portfolio></Portfolio>
+        </>
+        ) : (
+          <Contact></Contact>
+        )}
       </main>
       <Footer />
     </div>
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import headshotImage from "../../assets/main/BASphoto.jpg";
+import headshot from "../../assets/main/BASphoto.jpg";
 
 const About = () => {
     return (
         <section className="my-5">
             <h1 id="about">About Me</h1>
-            <img src={headshotImage} className="my-2" style={{ width: "100%" }} alt="photo of Bethany" />
+            <img src={headshot} className="my-2" style={{ width: "75%" }} alt="photo of Bethany" />
         </section>
     );
 
